6083d96aa442c4a1f7d8f8836b47496b643b7078
Aditya Lanka <87301182+adilanka@users.noreply.github.com>
2023-05-22 22:33:00 +0530

    Add files via upload

---
+++ Named Entity Recog.py
@@ -0,0 +1,8 @@
+import spacy
+from spacy import displacy
+
+NER = spacy.load("en_core_web_sm")
+rawText = "The Indian Space Research Organisation or is the national space agency of India, headquartered in Bengaluru. It operates under Department of Space which is directly overseen by the Prime Minister of India while Chairman of ISRO acts as executive of DOS as well."
+text1 = NER(rawText)
+for word in text1.ents:
+    print(word.text,word.label_)
